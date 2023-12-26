44a994e4ad46698f9fd8ecbe17b6c6bc69b5b7ed
LucasHBD <130990709+LucasHBD@users.noreply.github.com>
2023-12-26 10:54:11 -0300

    Midia.cs

--- projeto/Midia.cs
+++ projeto/Midia.cs
@@ -1,15 +1,8 @@
 using System;
 using System.Collections.Generic;
 
-    public class Midia{
-        public int Tipo {get; set;}
-        public string Titulo {get; set;}
-        public string Descricao {get; set;}
-        public string Autor {get; set;}
-        public static void Avaliar(Avaliacao av){
-
-        }
-        public static void MarcarComoVisto(bool marc){
-
-        }
+public class Filme{
+    public int Id {get; set;}
+    public string Nome {get; set;}
+    public string Tipo{get; set;}
 }
