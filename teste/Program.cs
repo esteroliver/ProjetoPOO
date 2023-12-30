9dfcd05d24ed98e92298dc652defaf0854a897bc
Ester Oliveira <oliver.ester.2004@gmail.com>
2023-12-30 17:35:51 -0300

    método do administrador

--- teste/Program.cs
+++ teste/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 
 class Program{
     public static void Main(){
+        Administrador();
         Console.WriteLine("TADSBoxD");
         int op = Login();
         switch(op){
@@ -38,6 +39,11 @@ class Program{
                     }
                 } break;
         }
+    }
+    public static void Administrador(){
+        Usuario user = new Usuario{Id = 1, Username = "admin", Senha = "IFRNtads2023"};
+        NUsuario usuarios = new NUsuario();
+        usuarios.Add(user);
     }
     public static int Login(){
         Console.WriteLine("1 - Cadastrar");
